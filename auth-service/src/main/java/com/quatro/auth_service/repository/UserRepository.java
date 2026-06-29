a811481518a8117f344a942c7779b4ed44602ec3
Tiago Almeida <tiagosilveira0477@gmail.com>
2026-06-29 01:23:21 -0300

    fix: Correção na assinatura da interface

--- auth-service/src/main/java/com/quatro/auth_service/repository/UserRepository.java
+++ auth-service/src/main/java/com/quatro/auth_service/repository/UserRepository.java
@@ -4,10 +4,12 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.quatro.auth_service.domain.entity.User;
 
-public interface UserRepository extends JpaRepository<UUID, User>{
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID>{
 
     public Optional<User> findByEmail(String email);
 
