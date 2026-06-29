1f20fb6300806f84af3c0018475e0ff2b6a5d292
Tiago Almeida <tiagosilveira0477@gmail.com>
2026-06-29 02:04:49 -0300

    fix: Correção de import da annotation @RequestBody em AuthController

--- auth-service/src/main/java/com/quatro/auth_service/controller/AuthController.java
+++ auth-service/src/main/java/com/quatro/auth_service/controller/AuthController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.quatro.auth_service.domain.dto.LoginRequestDto;
@@ -12,7 +13,6 @@ import com.quatro.auth_service.domain.dto.LoginResponseDto;
 import com.quatro.auth_service.service.AuthService;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 public class AuthController {
