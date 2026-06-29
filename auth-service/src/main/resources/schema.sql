f053f5b610bcd5d56df91e6adbbf8ec3fe160b0f
Tiago Almeida <tiagosilveira0477@gmail.com>
2026-06-29 01:21:21 -0300

    feat: Adição de script de banco de dados + configurações de inicialização de script

---
+++ auth-service/src/main/resources/schema.sql
@@ -0,0 +1,18 @@
+-- Script do banco de dados, executado sempre que o container inicia
+
+CREATE TABLE IF NOT EXISTS "users" (
+    id UUID PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    senha VARCHAR(255) NOT NULL,
+    cargo VARCHAR(16) NOT NULL
+);
+
+INSERT INTO "users" (id, email, senha, cargo)
+SELECT '2b0cb2b1-ed7f-43cd-93ee-4ca15cb3a72f', 'emailAdmin@admin.com',
+'$2b$12$7hoRZfJrRKD2nIm2vHLs7OBETy.LWenXXMLKf99W8M4PUwO6KB7fu', 'ADMIN'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM "users"
+    WHERE id='2b0cb2b1-ed7f-43cd-93ee-4ca15cb3a72f'
+    OR email='emailAdmin@admin.com'
+)
