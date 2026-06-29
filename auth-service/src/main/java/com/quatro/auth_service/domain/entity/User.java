40598ac279380b4e8fc69ed421984e7ecbe40d84
Tiago Almeida <tiagosilveira0477@gmail.com>
2026-06-29 01:22:32 -0300

    fix: Adicionada annotation @Entity e correção de import da annotation @Id

--- auth-service/src/main/java/com/quatro/auth_service/domain/entity/User.java
+++ auth-service/src/main/java/com/quatro/auth_service/domain/entity/User.java
@@ -2,11 +2,12 @@ package com.quatro.auth_service.domain.entity;
 
 import java.util.UUID;
 
-import org.springframework.data.annotation.Id;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,6 +18,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
 @Table(name="users")
 public class User {
 
