50aebfb72409770a0a2fecb4d08adf1fead23ff7
Tiago Almeida <tiagosilveira0477@gmail.com>
2026-06-28 23:53:09 -0300

    feat: Implementação camada de apresentação e rota POST 'login'

---
+++ auth-service/src/main/java/com/quatro/auth_service/controller/AuthController.java
@@ -0,0 +1,40 @@
+package com.quatro.auth_service.controller;
+
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.quatro.auth_service.domain.dto.LoginRequestDto;
+import com.quatro.auth_service.domain.dto.LoginResponseDto;
+import com.quatro.auth_service.service.AuthService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@RestController
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @Operation(summary = "Efetua login")
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDto> login(
+        @RequestBody LoginRequestDto loginRequestDto) {
+            Optional<String> tokenOptional = authService.authenticate(loginRequestDto);
+            if (tokenOptional.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            }
+            String token = tokenOptional.get();
+            return ResponseEntity.ok(new LoginResponseDto(token));
+    }
+
+
+
+}
