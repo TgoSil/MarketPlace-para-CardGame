1739f0e5ff7f97eed1f71b2d43b0746c9af7930b
Tiago Almeida <tiagosilveira0477@gmail.com>
2026-07-10 16:04:07 -0300

    feat: Adicionado atributo "username" à entidade User

--- auth-service/src/main/java/com/quatro/auth_service/domain/entity/User.java
+++ auth-service/src/main/java/com/quatro/auth_service/domain/entity/User.java
@@ -26,6 +26,9 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
     
+    @Column(nullable = false, unique = true)
+    private String username;
+
     @Column(nullable = false, unique=true)
     private String email;
 
