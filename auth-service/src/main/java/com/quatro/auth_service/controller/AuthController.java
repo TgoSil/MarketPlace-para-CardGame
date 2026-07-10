5f4e2d065ea2e05048332a4efa04c3c4d2a3e7bf
Tiago Almeida <tiagosilveira0477@gmail.com>
2026-07-10 16:09:33 -0300

    feat: Implementação de endpoint /register + Adição de @Valid para validação de DTOs

--- auth-service/src/main/java/com/quatro/auth_service/controller/AuthController.java
+++ auth-service/src/main/java/com/quatro/auth_service/controller/AuthController.java
@@ -12,9 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.quatro.auth_service.domain.dto.LoginRequestDto;
 import com.quatro.auth_service.domain.dto.LoginResponseDto;
+import com.quatro.auth_service.domain.dto.RegisterRequestDto;
 import com.quatro.auth_service.service.AuthService;
 
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
 
 @RestController
 public class AuthController {
@@ -28,7 +30,7 @@ public class AuthController {
     @Operation(summary = "Efetua login")
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDto> login(
-        @RequestBody LoginRequestDto loginRequestDto) {
+        @Valid @RequestBody LoginRequestDto loginRequestDto) {
             Optional<String> tokenOptional = authService.authenticate(loginRequestDto);
             if (tokenOptional.isEmpty()) {
                 return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
@@ -46,9 +48,13 @@ public class AuthController {
             }
             return authService.validateToken(authHeader.substring(7))
             ? ResponseEntity.ok().build() : ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-            
         }
 
-
-
+    @Operation(summary = "Registra usuário")
+    @PostMapping("/register")
+    public ResponseEntity<LoginResponseDto> register(
+        @Valid @RequestBody RegisterRequestDto registerRequestDto) {
+            String token = authService.register(registerRequestDto);
+            return ResponseEntity.status(HttpStatus.CREATED).body(new LoginResponseDto(token));
+        }
 }
