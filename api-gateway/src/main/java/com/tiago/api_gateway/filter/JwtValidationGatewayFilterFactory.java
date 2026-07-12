2c6edc49ab325ed746c7d7e971c3eb58c2ce2363
Tiago Almeida <tiagosilveira0477@gmail.com>
2026-07-12 20:00:38 -0300

    feat: Mudança de validação de token via webClient para validação local no JwtValidation do Api-gateway

--- api-gateway/src/main/java/com/tiago/api_gateway/filter/JwtValidationGatewayFilterFactory.java
+++ api-gateway/src/main/java/com/tiago/api_gateway/filter/JwtValidationGatewayFilterFactory.java
@@ -1,23 +1,24 @@
 package com.tiago.api_gateway.filter;
 
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.WebClient;
+
+import com.tiago.api_gateway.utils.JwtUtil;
+
+import io.jsonwebtoken.JwtException;
 
 @Component
 public class JwtValidationGatewayFilterFactory extends AbstractGatewayFilterFactory<Object>{
 
-    private final WebClient webClient;
+    private final JwtUtil jwtUtil;
 
-    public JwtValidationGatewayFilterFactory(WebClient.Builder webClientBuilder,
-        @Value("${auth.service.url}") String authServiceUrl) {
-            this.webClient = webClientBuilder.baseUrl(authServiceUrl).build();
-        }
+    public JwtValidationGatewayFilterFactory(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
 
 
     @Override
@@ -29,13 +30,14 @@ public class JwtValidationGatewayFilterFactory extends AbstractGatewayFilterFact
                 exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
                 return exchange.getResponse().setComplete();
             }
-
-            return webClient.get()
-                    .uri("/validate")
-                    .header(HttpHeaders.AUTHORIZATION, token)
-                    .retrieve()
-                    .toBodilessEntity()
-                    .then(chain.filter(exchange));
+            try {
+                String id = jwtUtil.validateToken(token.substring(7)).getSubject();
+                ServerHttpRequest modifiedRequest = exchange.getRequest().mutate().header("User-Id", id).build();
+                return chain.filter(exchange.mutate().request(modifiedRequest).build());
+            } catch(JwtException e) {
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
         };
     }
     
