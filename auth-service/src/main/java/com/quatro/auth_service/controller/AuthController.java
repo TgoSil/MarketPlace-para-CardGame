8e2ea0e19540054ff5709c5747c2f66cf46339a1
Tiago Almeida <tiagosilveira0477@gmail.com>
2026-07-12 19:57:28 -0300

    fix:  remoção de endpoint validate do controller + remoção de imports desnecessários

--- auth-service/src/main/java/com/quatro/auth_service/controller/AuthController.java
+++ auth-service/src/main/java/com/quatro/auth_service/controller/AuthController.java
@@ -4,10 +4,8 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.quatro.auth_service.domain.dto.LoginRequestDto;
@@ -39,16 +37,16 @@ public class AuthController {
             return ResponseEntity.ok(new LoginResponseDto(token));
     }
 
-    @Operation(summary = "Valida token do usuário")
-    @GetMapping("/validate")
-    public ResponseEntity<Void> validate(
-        @RequestHeader("Authorization") String authHeader) {
-            if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-            }
-            return authService.validateToken(authHeader.substring(7))
-            ? ResponseEntity.ok().build() : ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
+    // @Operation(summary = "Valida token do usuário")
+    // @GetMapping("/validate")
+    // public ResponseEntity<Void> validate(
+    //     @RequestHeader("Authorization") String authHeader) {
+    //         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+    //             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    //         }
+    //         return authService.validateToken(authHeader.substring(7))
+    //         ? ResponseEntity.ok().build() : ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    //     }
 
     @Operation(summary = "Registra usuário")
     @PostMapping("/register")
