5ed84126a11c80c0da177bceaf62252a27826627
Tiago Almeida <tiagosilveira0477@gmail.com>
2026-07-14 21:54:15 -0300

    refactor: Nome de evento de usuario alterado

--- auth-service/src/main/java/com/quatro/auth_service/domain/event/UsuarioEvent.java
+++
@@ -1,7 +0,0 @@
-package com.quatro.auth_service.domain.event;
-
-import java.util.UUID;
-
-public record UsuarioEvent(UUID id, String username) {
-    public static final String CRIADO = "CRIADO";
-}
